b3ae2cacb0a0569fe8f6387fa737c14cb0ae4ea8
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-09-23 13:57:14 +0300

    Add Korp test plugins test1 and test2 (Blueprint-based)

---
+++ korpplugins/test1/__init__.py
@@ -0,0 +1,32 @@
+
+"""
+korpplugins.test1
+
+Korp test plugin for a Blueprint- and function-based plugin proposal:
+endpoint /test and a result wrapper in a package with a separate
+configuration module.
+"""
+
+
+from korpplugins import Blueprint
+
+try:
+    from . import config as pluginconf
+except ImportError:
+    class pluginconf:
+        ARGS_NAME = "args_default"
+        WRAP_NAME = "wrap_default"
+
+
+test_plugin = Blueprint("test_plugin", __name__)
+
+
+@test_plugin.route("/test", extra_decorators=["test_decor"])
+def test(args):
+    """Yield arguments wrapped in ARGS_NAME."""
+    yield {pluginconf.ARGS_NAME: args}
+
+
+def filter_result(d):
+    """Wrap the result dictionary in WRAP_NAME."""
+    return {pluginconf.WRAP_NAME: d}
---
+++ korpplugins/test2.py
@@ -0,0 +1,15 @@
+
+"""
+korpplugins.test2
+
+Korp test plugin for a Blueprint- and function-based plugin proposal:
+a result wrapper as a stand-alone module.
+"""
+
+
+import korpplugins
+
+
+def filter_result(d):
+    """Wrap the result dictionary in "wrap2"."""
+    return {"wrap2": d}
