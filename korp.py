d15bcdf26654ced38b42f472fec27471467c6988
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-09-23 13:35:22 +0300

    korp.py: Add test_decor

--- korp.py
+++ korp.py
@@ -3386,8 +3386,21 @@ if config.MEMCACHED_SERVERS and not cache_disabled:
 # Set up caching
 setup_cache()
 
+
+def test_decor(generator):
+    """A decorator for testing specifying extra decorators in WSGI
+    endpoint plugins."""
+    @functools.wraps(generator)
+    def decorated(args=None, *pargs, **kwargs):
+        for x in generator(args, *pargs, **kwargs):
+            yield {"test_decor": "Endpoint decorated with test_decor",
+                   "payload": x}
+    return decorated
+
+
 # Load plugins
-korpplugins.load(app, config.PLUGINS, main_handler, [prevent_timeout])
+korpplugins.load(app, config.PLUGINS, main_handler,
+                 [prevent_timeout, test_decor])
 
 
 if __name__ == "__main__":
