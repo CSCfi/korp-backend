b62864fe80015ee2109698340bd089428de465e8
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-10-15 13:45:07 +0300

    Configurable collation for lemgram data
    Make the MySQL database collation for the lemgram data configurable via
    config.DBCOLLATE_LEMGRAM, as the collation utf8_bin can be used only if
    the database character set is utf8 (and not e.g. utf8mb4).

--- korp.py
+++ korp.py
@@ -2322,7 +2322,7 @@ def lemgram_count(args):
     corpora_sql = " AND corpus IN (%s)" % ", ".join("'%s'" % sql_escape(c) for c in corpora) if corpora else ""
 
     sql = "SELECT lemgram, " + sums + " AS freq FROM lemgram_index WHERE" + lemgram_sql + corpora_sql + \
-          " GROUP BY lemgram COLLATE utf8_bin;"
+          " GROUP BY lemgram COLLATE " + config.DBCOLLATE_LEMGRAM + ";"
 
     result = {}
     with app.app_context():
@@ -2707,14 +2707,14 @@ def relations(args):
                 selects.append((corpus.upper(),
                                 "(SELECT S1.string AS head, S1.pos AS headpos, F.rel, S2.string AS dep, S2.pos AS deppos, S2.stringextra AS depextra, F.freq, R.freq AS rel_freq, HR.freq AS head_rel_freq, DR.freq AS dep_rel_freq, " + corpus_sql + " AS corpus, F.id " +
                                 "FROM `" + corpus_table + "_strings` AS S1, `" + corpus_table + "_strings` AS S2, `" + corpus_table + "` AS F, `" + corpus_table + "_rel` AS R, `" + corpus_table + "_head_rel` AS HR, `" + corpus_table + "_dep_rel` AS DR " +
-                                "WHERE S1.string = " + lemgram_sql + " COLLATE utf8_bin AND F.head = S1.id AND S2.id = F.dep " +
+                                "WHERE S1.string = " + lemgram_sql + " COLLATE " + config.DBCOLLATE_LEMGRAM + " AND F.head = S1.id AND S2.id = F.dep " +
                                 minfreqsql +
                                 "AND F.bfhead = 1 AND F.bfdep = 1 AND F.rel = R.rel AND F.head = HR.head AND F.rel = HR.rel AND F.dep = DR.dep AND F.rel = DR.rel)"
                                 ))
                 selects.append((None,
                                 "(SELECT S1.string AS head, S1.pos AS headpos, F.rel, S2.string AS dep, S2.pos AS deppos, S2.stringextra AS depextra, F.freq, R.freq AS rel_freq, HR.freq AS head_rel_freq, DR.freq AS dep_rel_freq, " + corpus_sql + " AS corpus, F.id " +
                                 "FROM `" + corpus_table + "_strings` AS S1, `" + corpus_table + "_strings` AS S2, `" + corpus_table + "` AS F, `" + corpus_table + "_rel` AS R, `" + corpus_table + "_head_rel` AS HR, `" + corpus_table + "_dep_rel` AS DR " +
-                                "WHERE S2.string = " + lemgram_sql + " COLLATE utf8_bin AND F.dep = S2.id AND S1.id = F.head " +
+                                "WHERE S2.string = " + lemgram_sql + " COLLATE " + config.DBCOLLATE_LEMGRAM + " AND F.dep = S2.id AND S1.id = F.head " +
                                 minfreqsql +
                                 "AND F.bfhead = 1 AND F.bfdep = 1 AND F.rel = R.rel AND F.head = HR.head AND F.rel = HR.rel AND F.dep = DR.dep AND F.rel = DR.rel)"
                                 ))
