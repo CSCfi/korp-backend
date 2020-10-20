a39b480fa9150e2fa40666ae891cf9a86c6f8b99
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-10-20 13:15:30 +0300

    korpplugins.test2: Add Test3: all plugin mount points

--- korpplugins/test2.py
+++ korpplugins/test2.py
@@ -7,6 +7,8 @@ stand-alone module.
 """
 
 
+import traceback
+
 import korpplugins
 
 
@@ -14,3 +16,32 @@ class Test2(korpplugins.KorpFunctionPlugin):
 
     def filter_result(self, d, *rest):
         return {"wrap2": d}
+
+
+class Test3(korpplugins.KorpFunctionPlugin):
+
+    """Print the arguments at all plugin mount points"""
+
+    def enter_handler(self, args, starttime, *rest):
+        print("enter_handler", args, starttime, *rest)
+
+    def exit_handler(self, endtime, elapsed, *rest):
+        print("exit_handler", endtime, elapsed, *rest)
+
+    def error(self, error, exc, *rest):
+        print("error", error, traceback.format_exception(*exc))
+
+    def filter_args(self, args, *rest):
+        print("filter_args", args, *rest)
+
+    def filter_result(self, result, *rest):
+        print("filter_result", result, *rest)
+
+    def filter_cqp_input(self, cmd, *rest):
+        print("filter_cqp_input", cmd, *rest)
+
+    def filter_cqp_output(self, output, *rest):
+        print("filter_cqp_output", output, *rest)
+
+    def filter_sql(self, sql, *rest):
+        print("filter_sql", sql, *rest)
