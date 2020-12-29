cb87af396fe4031744d5729af9f2c05cdd22d3cf
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-12-29 16:39:32 +0200

    korppluginlib: Function plugin -> callback plugin
    korppluginlib:
    - Adjust terminology: "function plugin" -> "callback plugin", "plugin
      function" -> "callback (method)", "mount point" -> "(plugin) hook
      point".
    
    korppluginlib/_callbackplugin.py:
    - Rename KorpFunctionPluginMetaclass -> KorpCallbackPluginMetaclass,
      KorpFunctionPlugin -> KorpCallbackPlugin, KorpFunctionPluginCaller ->
      KorpCallbackPluginCaller.
    - Renamed arguments and variables accordingly.
    - Adjusted the terminology in the messages printed when loading plugins
      accordingly.
    - Edited docstrings accordingly.
    - Separated the first line in docstrings that did not yet have a summary
      line.

--- korppluginlib/__init__.py
+++ korppluginlib/__init__.py
@@ -5,9 +5,9 @@ Package korppluginlib
 A class- and Blueprint-based proposal for a plugin framework for the Korp
 backend
 
-The classes are mostly for encapsulation: although the plugin functions are
-instance methods, the instances are singletons. Registering plugin functions
-(for KorpFunctionPlugin subclasses) is handled in the metaclass, adapted from
+The classes are mostly for encapsulation: although plugin callbacks are
+instance methods, the instances are singletons. Registering plugin callbacks
+(for KorpCallbackPlugin subclasses) is handled in the metaclass, adapted from
 or inspired by http://martyalchin.com/2008/jan/10/simple-plugin-framework/
 
 WSGI endpoints are functions decorated with the route method of an instance of
@@ -23,5 +23,5 @@ korppluginlib.Blueprint.
 # The following names are visible to the code importing korppluginlib
 from ._configutil import get_plugin_config, plugin_configs
 from ._endpointplugin import Blueprint
-from ._callbackplugin import KorpFunctionPlugin, KorpFunctionPluginCaller
+from ._callbackplugin import KorpCallbackPlugin, KorpCallbackPluginCaller
 from ._pluginloader import load, app_globals, loaded_plugins
--- korppluginlib/_callbackplugin.py
+++ korppluginlib/_callbackplugin.py
@@ -2,11 +2,11 @@
 """
 Module korppluginlib._callbackplugin
 
-Module containing code for function plugins to be called at mount points
+Module containing code for callback plugins to be called at hook points
 
-The classes are mostly for encapsulation: although the plugin functions are
-instance methods, the instances are singletons. Registering plugin functions in
-KorpFunctionPlugin subclasses is handled in the metaclass, adapted from or
+The classes are mostly for encapsulation: although the callbacks are instance
+methods, the instances are singletons. Registering callbacks in
+KorpCallbackPlugin subclasses is handled in the metaclass, adapted from or
 inspired by http://martyalchin.com/2008/jan/10/simple-plugin-framework/ .
 
 This module is intended to be internal to the package korppluginlib; the names
@@ -36,22 +36,23 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-class KorpFunctionPluginMetaclass(Singleton):
+class KorpCallbackPluginMetaclass(Singleton):
 
-    """This metaclass takes care of registering the plugin functions
-    (methods) in the plugin classes (subclasses of
-    KorpFunctionPlugin); see
+    """Metaclass for KorpCallbackPlugin, registering callbacks
+
+    This metaclass takes care of registering the callback methods in the
+    plugin classes (subclasses of KorpCallbackPlugin); see
     http://martyalchin.com/2008/jan/10/simple-plugin-framework/
     """
 
     def __init__(cls, name, bases, attrs):
-        if not hasattr(cls, "_plugin_funcs"):
+        if not hasattr(cls, "_callbacks"):
             # This executes when initializing the base class for the
             # first time
-            cls._plugin_funcs = defaultdict(list)
-            # Methods defined in the base class are not plugin functions:
+            cls._callbacks = defaultdict(list)
+            # Methods defined in the base class are not callback methods:
             # record them here so that they can be excluded when registering
-            # plugin functions in subclasses
+            # callback methods in subclasses
             cls._base_methods = [name for name in attrs]
             # List of subclasses
             cls._subclasses = []
@@ -64,18 +65,20 @@ class KorpFunctionPluginMetaclass(Singleton):
                 attr = getattr(inst, name)
                 if (name[0].islower() and callable(attr)
                         and name not in cls._base_methods):
-                    cls._plugin_funcs[name].append((attr, cls.applies_to))
+                    cls._callbacks[name].append((attr, cls.applies_to))
                     print_verbose(
-                        2, ("  mount point \"" + name
-                            + "\": function " + attr.__qualname__))
+                        2, ("  hook point \"" + name
+                            + "\": callback " + attr.__qualname__))
 
 
-class KorpFunctionPlugin(metaclass=KorpFunctionPluginMetaclass):
+class KorpCallbackPlugin(metaclass=KorpCallbackPluginMetaclass):
 
-    """Actual function plugin classes are subclasses of this class, with
-    plugin functions as instance methods. All methods whose names begin
-    with a lowercase letter are treated as plugin functions for the
-    plugin mount point of the method name.
+    """Base class for callback plugins
+
+    Actual callback plugin classes are subclasses of this class, with
+    callbacks as instance methods. All methods whose names begin with a
+    lowercase letter are treated as callback methods for the hook point
+    of the method name.
     """
 
     @classmethod
@@ -83,27 +86,27 @@ class KorpFunctionPlugin(metaclass=KorpFunctionPluginMetaclass):
         """Return True if the plugin should be applied to request.
 
         This method always returns True, so it should be overridden in
-        plugin function classes which restrict their applicability
+        callback plugin classes which restrict their applicability
         based on Flask request information, typically the endpoint.
         """
         return True
 
 
-class KorpFunctionPluginCaller:
+class KorpCallbackPluginCaller:
 
-    """Class for calling plugin functions at named mount points.
+    """Class for calling plugin callbacks at named hook points.
 
     This class should be instantiated once for each Flask request. The
     call methods take care of passing the actual request object to the
-    plugin functions, so that it need not be specified at every call.
+    callback methods, so that it need not be specified at every call.
     """
 
     # Instances of this class: dict[request object id,
-    # KorpFunctionPluginCaller]
+    # KorpCallbackPluginCaller]
     _instances = {}
 
     def __init__(self, request=None):
-        """Initialize a KorpFunctionPluginCaller with the given request.
+        """Initialize a KorpCallbackPluginCaller with the given request.
 
         If request is None, use the object for the global request proxy.
         """
@@ -132,81 +135,88 @@ class KorpFunctionPluginCaller:
         return cls._instances[id(request_obj)]
 
     def cleanup(self):
-        """Clean up when this KorpFunctionPluginCaller is no longer used."""
+        """Clean up when this KorpCallbackPluginCaller is no longer used."""
         # Remove self from _instances
         del self._instances[id(self._request)]
 
-    def call(self, mount_point, *args, **kwargs):
-        """Call the plugins in mount_point, discarding return values
+    def call(self, hook_point, *args, **kwargs):
+        """Call the callbacks for hook_point, discarding return values.
 
-        Call the plugins in mount_point with args and kwargs in
-        sequence, discarding return values. Plugins whose applies_to
-        method returns false for the current request are skipped.
+        Call the callback methods registered for hook_point with args
+        and kwargs in sequence, discarding return values. Callback
+        methods in plugin classes whose applies_to method returns
+        false for the current request are skipped.
         """
-        for func, applies_to in (KorpFunctionPlugin
-                                 ._plugin_funcs.get(mount_point, [])):
+        for callback, applies_to in (KorpCallbackPlugin
+                                    ._callbacks.get(hook_point, [])):
             if applies_to(self._request):
-                func(*args, self._request, **kwargs)
+                callback(*args, self._request, **kwargs)
 
-    def call_collect(self, mount_point, *args, **kwargs):
-        """Call the plugins in mount_point, collecting return values to a list
+    def call_collect(self, hook_point, *args, **kwargs):
+        """Call the callbacks for hook_point; collect return values to a list.
 
-        Call the plugins in mount_point with args and kwargs in sequence,
-        collect their return values to a list and return it. Return values
-        None are ignored. Plugins whose applies_to method returns
+        Call the callback methods registered for hook_point with args
+        and kwargs in sequence, collect their return values to a list
+        and return it. Return values None are ignored. Callback
+        methods in plugin classes whose applies_to method returns
         false for the current request are skipped.
         """
         result = []
-        for func, applies_to in (KorpFunctionPlugin
-                                 ._plugin_funcs.get(mount_point, [])):
+        for callback, applies_to in (KorpCallbackPlugin
+                                     ._callbacks.get(hook_point, [])):
             if applies_to(self._request):
-                retval = func(*args, self._request, **kwargs)
+                retval = callback(*args, self._request, **kwargs)
                 if retval is not None:
                     result.append(retval)
         return result
 
-    def call_chain(self, mount_point, arg1, *args, **kwargs):
-        """Call the plugins in mount_point, passing return value to the next
+    def call_chain(self, hook_point, arg1, *args, **kwargs):
+        """Call the callbacks for hook_point, passing return value to the next.
 
-        Return the value of arg1 as passed through the plugins in
-        mount_point, with the return value of the preceding plugin
-        function as the arg1 value of the following one, unless it is
-        None, in which case arg1 is kept as is. *args and **kwargs are
-        passed to each function as they are. Plugins whose applies_to
-        method returns false for the current request are skipped.
+        Return the value of arg1 as passed through the callback
+        methods registered for hook_point, with the return value of
+        the preceding callback as the arg1 value of the following one,
+        unless it is None, in which case arg1 is kept as is. *args and
+        **kwargs are passed to each callback method as they are.
+        Callback methods in plugin classes whose applies_to method
+        returns false for the current request are skipped.
+
+        This can be thought of as applying successive filters,
+        function composition for the callbacks, or a reduce operation
+        for functions ("freduce").
         """
-        for func, applies_to in (KorpFunctionPlugin
-                                 ._plugin_funcs.get(mount_point, [])):
+        for callback, applies_to in (KorpCallbackPlugin
+                                     ._callbacks.get(hook_point, [])):
             if applies_to(self._request):
-                retval = func(arg1, *args, self._request, **kwargs)
+                retval = callback(arg1, *args, self._request, **kwargs)
                 if retval is not None:
                     arg1 = retval
         return arg1
 
     @classmethod
-    def call_for_request(cls, mount_point, *args, request=None, **kwargs):
-        """Call the plugins in mount_point for request.
+    def call_for_request(cls, hook_point, *args, request=None, **kwargs):
+        """Call the callbacks for hook_point for request.
 
         If request is None, use the global request proxy.
         """
-        cls.get_instance(request).call(mount_point, *args, **kwargs)
+        cls.get_instance(request).call(hook_point, *args, **kwargs)
 
     @classmethod
-    def call_collect_for_request(cls, mount_point, *args, request=None,
+    def call_collect_for_request(cls, hook_point, *args, request=None,
                                  **kwargs):
-        """Call the plugins in mount_point for request, collecting to a list.
+        """Call the callbacks for hook_point for request, collecting to a list.
 
         If request is None, use the global request proxy.
         """
         return cls.get_instance(request).call_collect(
-            mount_point, *args, **kwargs)
+            hook_point, *args, **kwargs)
 
     @classmethod
-    def call_chain_for_request(cls, mount_point, arg1, *args, request=None,
+    def call_chain_for_request(cls, hook_point, arg1, *args, request=None,
                                **kwargs):
-        """Call the plugins in mount_point for request, passing return value
+        """Call the callbacks for hook_point for request, passing return value.
 
         If request is None, use the global request proxy.
         """
         return cls.get_instance(request).call_chain(
-            mount_point, arg1, *args, **kwargs)
+            hook_point, arg1, *args, **kwargs)
