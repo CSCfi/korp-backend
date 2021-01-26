c1151f21f3d56167e8c142a6f67aa4ca660395b0
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-01-26 14:49:44 +0200

    korpplugins.lemgramcompleter: Add info module

---
+++ korpplugins/lemgramcompleter/info.py
@@ -0,0 +1,13 @@
+
+"""
+korpplugins.lemgramcompleter.info
+
+Information on the lemgramcompleter Korp plugin.
+"""
+
+
+NAME = "Endpoint /lemgram_complete"
+VERSION = "0.3"
+DATE = "2021-01-26"
+AUTHOR = "Jyrki Niemi / FIN-CLARIN"
+DESCRIPTION = "Implement /lemgram_complete endpoint, to find lemgram completions for a prefix."
