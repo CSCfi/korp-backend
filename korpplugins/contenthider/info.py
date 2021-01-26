66c5f67ae7d15cdaae950dc658dd6afe6b67877c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-01-26 14:56:57 +0200

    korpplugins.contenthider: Add info module

---
+++ korpplugins/contenthider/info.py
@@ -0,0 +1,13 @@
+
+"""
+korpplugins.contenthider.info
+
+Information on the contenthider Korp plugin.
+"""
+
+
+NAME = "KWIC content hider"
+VERSION = "0.1"
+DATE = "2021-01-07"
+AUTHOR = "Jyrki Niemi / FIN-CLARIN"
+DESCRIPTION = "Hide marked structures in KWIC results by replacing their attribute values with specified fixed strings."
